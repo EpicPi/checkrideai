666436d51b45da7b56a3de53fc1dca9e2e423cb1
Piyush <piyushgk1@gmail.com>
2023-11-15 13:46:42 -0800

    fix: add baseplugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   return {
     pathPrefix: "/checkrideai/",
     dir: {
