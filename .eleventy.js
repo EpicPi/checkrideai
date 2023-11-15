697b1c2f0d227334265eefc45df34233dd2a0281
Piyush <piyushgk1@gmail.com>
2023-11-15 13:56:09 -0800

    chore: move to checkrideai-web

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   return {
-    pathPrefix: "/checkrideai/",
+    pathPrefix: "/checkrideai-web/",
     dir: {
       input: "src",
       output: "dist",
